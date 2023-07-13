a1e9107fe5fa430c9bde4c4de128ce7241df14ba
joejoturkson89 <turksonjoejo@gmail.com>
2023-07-13 06:47:33 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,9 @@
  */
 
 int main(void)
-{ 
-       puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);       
+{
+       puts("\"Programming is like building a multilingual puzzle"); 
+       return (0);        
 
 
 }
