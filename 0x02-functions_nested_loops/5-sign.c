33eb797e1f613698e215113497e757658e5a1f68
joejoturkson89 <turksonjoejo@gmail.com>
2023-07-18 08:31:46 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
 	}
-	_putchar('_');
+	_putchar(45);
 	return (-1);
 }
