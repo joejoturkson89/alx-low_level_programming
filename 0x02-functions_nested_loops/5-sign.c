3845932c64ae649379f95d236e83122a24281e89
joejoturkson89 <turksonjoejo@gmail.com>
2023-07-18 08:46:46 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,14 +5,14 @@
  * Return: 1 for positive num,0 for zero or -1 for negative
  */
 
-int print_sign(int n)
+int print_sign(int i)
 {
-	if (n > 0)
+	if (i > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	if (n == 0)
+	if (i == 0)
 	{
 		_putchar(48);
 		return (0);
