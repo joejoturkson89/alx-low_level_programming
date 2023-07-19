9ebeae845e7044c4016ebd3617d387405c78fd15
joejoturkson89 <turksonjoejo@gmail.com>
2023-07-19 05:25:17 -0700

    1 main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinity loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinity loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*      putchar(i);*/
+	/*}*/
+
+	printf("Infinity loop avoided! \\o/\n");
+
+	return (0);
+}
