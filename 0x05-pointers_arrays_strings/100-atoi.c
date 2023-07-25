039dc795d713c1bbc0f4df3972f549f0c1b1ea32
joejoturkson89 <turksonjoejo@gmail.com>
2023-07-24 17:50:42 -0700

    100 atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,30 @@
 #include "main.h"
-#include "2-strlen.c"
 /**
- * _atoi - converts string to integer
- * @s: string to convert
- *
- * Return: returns integer value
- */
+* _atoi - Convert string to an integer.
+* @s: Pointer to a character string.
+*
+* Return: void.
+*/
 int _atoi(char *s)
 {
-	int i;
-	int np = 0;
-	int c;
-	int d = 1;
-	int num = 0;
-
-	for (i = 0; i < _strlen(s); i++)
-	{
-		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
-			break;
-		if (s[i] == '-')
-			np--;
-		if (s[i] == '+')
-			np++;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			c++;
-		}
-	}
-	while (c > 0)
-	{
-		num += ((s[i - 1] - '0') * d);
-		i--;
-		c--;
-		d *= 10;
-	}
-	if (np >= 0)
-	{
-		num *= 1;
-	} else
-	{
-		num *= -1;
-	}
-	return (num);
+int sign;
+unsigned int num;
+char *temp;
+temp = s;
+num = 0;
+sign = 1;
+while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+{
+if (*temp == '-')
+sign = sign * -1;
+temp++;
+}
+if (*temp != '\0')
+{
+do {
+num = num * 10 + (*temp - '0');
+temp++;
+} while (*temp >= '0' && *temp <= '9');
+}
+return (num * sign);
 }
