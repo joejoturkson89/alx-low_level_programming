3cde5e2669c4ebed1f745593d93b311fa7174008
joejoturkson89 <turksonjoejo@gmail.com>
2023-07-31 14:10:16 -0700

    8 print diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints diagonal summeries
+ * @a: arrays to use
+ * @size: sizes of diagonals
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int diagO = 0;
+	int diagT = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		diagO += a[(i * size) + i];
+		diagT += a[(size - 1) + ((size - 1) * i)];
+	}
+	printf("%d, %d\n", diagO, diagT);
+}
