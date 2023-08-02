51c64e37fba24ad36275e33c7a691d03250fb01b
joejoturkson89 <turksonjoejo@gmail.com>
2023-08-02 06:47:48 -0700

    0 puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - uses recursion
+ * @s: string to be recursed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar('\n');
+}
