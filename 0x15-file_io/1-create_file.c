630632363e7de8cb4b82088869be713dc2d49eb0
joejoturkson89 <turksonjoejo@gmail.com>
2023-09-04 14:14:35 -0700

    1 create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fild = 0, chk = 0, n = 0;
+	int ij, len = 0;
+	long int wrt;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
-	if (fild > 0)
+	ij = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (ij == -1)
 		return (-1);
-
-	while (text_content[n])
-		n++;
-	chk = write(fild, text_content, n);
-		if (chk < 0)
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		wrt = write(ij, text_content, len);
+		if (wrt == -1)
 			return (-1);
+	}
 
-				close(fild);
-				return (1);
+	if (close(ij) == -1)
+		return (-1);
+	return (1);
 }
